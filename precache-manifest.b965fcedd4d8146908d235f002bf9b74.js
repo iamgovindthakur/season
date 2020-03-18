41f9896b749445e028ebd7e0310771cd8a208c3a
Govind Thakur <iamgovindthakur@gmail.com>
2020-03-18 22:15:42 +0530

    Updates

---
+++ precache-manifest.b965fcedd4d8146908d235f002bf9b74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129e576898c46d35930a5b3850dd6799",
+    "url": "/season/index.html"
+  },
+  {
+    "revision": "4cb8597fa286dcbd8352",
+    "url": "/season/static/css/main.1b004983.chunk.css"
+  },
+  {
+    "revision": "c24b1b304a4bc9624974",
+    "url": "/season/static/js/2.28885a1b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/season/static/js/2.28885a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb8597fa286dcbd8352",
+    "url": "/season/static/js/main.8f91730b.chunk.js"
+  },
+  {
+    "revision": "1e14f8367574a49855d8",
+    "url": "/season/static/js/runtime-main.0ea5d469.js"
+  }
+]);
